de6e634474ea434c19e65c15d283497d04c82205
keerthana2356 <keerthanad2012@gmail.com>
2022-12-14 20:10:33 -0500

    Primitive data types

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,32 @@
+package dataTypes;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		//Integer
+		int myMinIntValue= 	Integer.MIN_VALUE;
+		int myMaxIntValue=  Integer.MAX_VALUE;
+		int exampleInteger=6789;
+		System.out.println(myMinIntValue);
+		System.out.println(myMaxIntValue);
+		System.out.println(exampleInteger);
+		//Short
+		short myShortValue = 2;
+		System.out.println("The short variable is " + myShortValue);
+		//Byte
+	    byte myByteValue = 6;
+	    System.out.println("The byte variable is " + myByteValue);
+	    //Long
+	    long myLongValue = 125362133223l;
+	    System.out.println("The long variable is " + myLongValue);
+	    //Float
+	    float myFloatValue = 65.20298f;
+	    System.out.println("The float variable is " + myFloatValue);
+	    //Double
+	    double d = 876.765d;
+	    System.out.println("The double variable is " + d);
+		
+
+	}
+
+}
